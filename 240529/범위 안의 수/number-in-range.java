cc415f245b0aeed63c8c55abe46d83f7fca4c09b
KIMDWAEJI <loundo001@gmail.com>
2024-05-29 07:16:32 +0900

    Title: 범위 안의 수, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240529/범위 안의 수/number-in-range.java
+++ 240529/범위 안의 수/number-in-range.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt(), b = sc.nextInt();
+		int a = sc.nextInt();
 
         if(a >= 10 && a <= 20) {
             System.out.println("yes");
