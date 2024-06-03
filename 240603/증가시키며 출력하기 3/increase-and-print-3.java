fa9431a4bcfce3f89bf6c2153d7d352369aca31a
KIMDWAEJI <loundo001@gmail.com>
2024-06-03 23:41:34 +0900

    Title: 증가시키며 출력하기 3, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240603/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 26;
+        int i = 10;
+
+        while(i <= n) {
+            System.out.print(i + " ");
+            i += 2;
+        }
+		
+    }
+}
