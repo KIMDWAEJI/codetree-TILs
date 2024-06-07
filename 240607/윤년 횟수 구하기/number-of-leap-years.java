87ab20c36e7ed643b941b06976f5871f91d2e8e5
KIMDWAEJI <loundo001@gmail.com>
2024-06-07 19:52:32 +0900

    Title: 윤년 횟수 구하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240607/윤년 횟수 구하기/number-of-leap-years.java
+++ 240607/윤년 횟수 구하기/number-of-leap-years.java
@@ -7,9 +7,7 @@ public class Main {
         int cnt = 0;
 
         for(int i = 1; i <= n; i++) {
-            if(i % 4 != 0 || (i % 100 == 0 && i % 400 != 0)) {
-                continue;
-            } else {
+            if((i % 4 == 0 && i % 100 != 0) || i % 400 == 0) {
                 cnt++;
             }
         }
