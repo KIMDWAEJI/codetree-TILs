0d869198d40b50c9878bd8f6d81f1054521bbcbc
KIMDWAEJI <loundo001@gmail.com>
2024-06-07 18:41:55 +0900

    Title: 369 게임, Time: 204ms, Memory: 10MB, Status: Passed - CodeTree

--- 240607/369 게임/369-game.java
+++ 240607/369 게임/369-game.java
@@ -4,29 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String[] str = new String[3];
 
         for(int i = 1; i <= n; i++) {
-            str = Integer.toString(i).split("");
-            
-            if(i < 10) {
-            	if(i % 3 == 0) {
-                	System.out.print("0 ");
-                } else if(str[0].equals("3") || str[0].equals("6") || str[0].equals("9")) {
-                	System.out.print("0 ");
-                } else {
-                	System.out.print(i + " ");
-                }
-            }
-            
-            if(i >= 10) {
-            	if(i % 3 == 0) {
-                	System.out.print("0 ");
-                } else if(str[0].equals("3") || str[0].equals("6") || str[0].equals("9") || str[1].equals("3") || str[1].equals("6") || str[1].equals("9")) {
-            		System.out.print("0 ");            		
-            	} else {
-            		System.out.print(i + " ");            		
-            	}
+            if(i % 3 == 0) {
+            	System.out.print("0 ");
+            } else if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+            	System.out.print("0 ");
+            } else if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
+            	System.out.print("0 ");
+            } else {
+            	System.out.print(i + " ");
             }
         }
 
