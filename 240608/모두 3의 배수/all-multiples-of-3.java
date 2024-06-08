b50bc08eadf2be4804d8234e58cbbdd598f84d1e
KIMDWAEJI <loundo001@gmail.com>
2024-06-08 16:03:06 +0900

    Title: 모두 3의 배수, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240608/모두 3의 배수/all-multiples-of-3.java
+++ 240608/모두 3의 배수/all-multiples-of-3.java
@@ -4,16 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean check = true;
+        int cnt = 0;
 
         for(int i = 0; i < 5; i++) {
             int n = sc.nextInt();
 
-            if(n % 3 == 0) {
+            if(n % 3 != 0) {
                 check = false;
             }
         }
 
-        if(check == false) {
+        if(check == true) {
             System.out.println("1");
         } else {
             System.out.println("0");
